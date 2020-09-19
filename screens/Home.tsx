66eedf8c62c23b0485b6b264f3cfcaeac6588122
Viktor Koncz <konczviktor@gmail.com>
2020-09-19 14:42:15 +0200

    Fetch colors

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,57 +1,40 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
-import { RootStackParamList, HexColor } from '../App';
+import { RootStackParamList, ColorPalette } from '../App';
 import { PalettePreview } from '../components/PalettePreview';
-
-const SOLARIZED: HexColor[] = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
-
-const RAINBOW = [
-  { colorName: 'Red', hexCode: '#FF0000' },
-  { colorName: 'Orange', hexCode: '#FF7F00' },
-  { colorName: 'Yellow', hexCode: '#FFFF00' },
-  { colorName: 'Green', hexCode: '#00FF00' },
-  { colorName: 'Violet', hexCode: '#8B00FF' },
-];
-
-const FRONTEND_MASTERS = [
-  { colorName: 'Red', hexCode: '#c02d28' },
-  { colorName: 'Black', hexCode: '#3e3e3e' },
-  { colorName: 'Grey', hexCode: '#8a8a8a' },
-  { colorName: 'White', hexCode: '#ffffff' },
-  { colorName: 'Orange', hexCode: '#e66225' },
-];
-
-const COLOR_PALETTES = [
-  { paletteName: 'Solarized', colors: SOLARIZED },
-  { paletteName: 'Frontend Masters', colors: FRONTEND_MASTERS },
-  { paletteName: 'Rainbow', colors: RAINBOW },
-];
 
 type Props = StackScreenProps<RootStackParamList, 'Home'>;
 
 export const Home: React.FC<Props> = ({ navigation }) => {
+  const [colorPalettes, setColorPalettes] = useState<ColorPalette[]>([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const fetchColorPalettes = useCallback(async () => {
+    const result = await fetch(
+      'https://color-palette-api.kadikraman.now.sh/palettes',
+    );
+
+    if (result.ok) {
+      const palettes: ColorPalette[] = await result.json();
+      setColorPalettes(palettes);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchColorPalettes();
+  }, [fetchColorPalettes]);
+
+  const handleRefresh = useCallback(async () => {
+    setIsRefreshing(true);
+    await fetchColorPalettes();
+    setIsRefreshing(false);
+  }, [fetchColorPalettes]);
+
   return (
     <FlatList
       style={styles.list}
-      data={COLOR_PALETTES}
+      data={colorPalettes}
       keyExtractor={(item) => item.paletteName}
       renderItem={({ item }) => (
         <PalettePreview
@@ -61,6 +44,8 @@ export const Home: React.FC<Props> = ({ navigation }) => {
           colorPalette={item}
         />
       )}
+      refreshing={isRefreshing}
+      onRefresh={handleRefresh}
     />
   );
 };
